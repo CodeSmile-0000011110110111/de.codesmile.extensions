87217529fcf201572f4096845382d8ef4915c2ee
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2024-02-28 19:23:01 +0100

    add ChildrenRenderOnlyShadows add MonoSingleton add ByteArrayExt GetString()

---
+++ Runtime/Components/MonoSingleton.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (C) 2021-2024 Steffen Itterheim
+// Refer to included LICENSE file for terms and conditions.
+
+using UnityEngine;
+
+namespace CodeSmile.Components
+{
+	public abstract class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+	{
+		private static T s_Instance;
+
+		public static T Singleton
+		{
+			get
+			{
+				if (s_Instance == null)
+				{
+					var go = new GameObject($"{nameof(T)}", typeof(T));
+					s_Instance = go.GetComponent<T>();
+					Debug.Log($"Instance is: {s_Instance}");
+				}
+
+				return s_Instance;
+			}
+		}
+	}
+}
